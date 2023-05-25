368eb41c314a97c2c185d68073dd5020fa697063
nahcg <glor.chan@mail.utoronto.ca>
2023-05-25 19:11:24 -0400

    add command-line args function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,8 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+let input = process.argv.slice(2)
+
+request('https://api.thecatapi.com/v1/breeds/search?q=' + input, (error, response, body) => {
   // Print the error if one occurred
   if (error !== null) {
   console.log('error:', error)

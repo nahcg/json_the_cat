e3b56ccd2abb0dbda258cc749fc0fbba2c9ff5a9
nahcg <glor.chan@mail.utoronto.ca>
2023-05-25 22:50:20 -0400

    fix loop and add edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,12 +5,16 @@ let input = process.argv.slice(2)
 request('https://api.thecatapi.com/v1/breeds/search?q=' + input, (error, response, body) => {
   // Print the error if one occurred
   if (error !== null) {
-  console.log('error:', error)
-  }; 
+    console.log('error:', error)
+  } else if (response.statusCode !== 200) {
   // Print the response status code if not 200
-  if (response.statusCode !== 200) {
-  console.log('statusCode:', response && response.statusCode)
+    console.log('statusCode:', response && response.statusCode);
+  } else if (typeof JSON.parse(body)[0] === 'undefined') {
+  // if no breed found 
+    console.log("No such breed")
+  } else {
+  // return desc
+    const data = JSON.parse(body);
+    console.log(data[0].description);
   };
-  const data = JSON.parse(body);
-  console.log(data[0].description);
 });

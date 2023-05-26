6320b1d2da88f53984944b90e100b5648eddea67
nahcg <glor.chan@mail.utoronto.ca>
2023-05-26 15:44:48 -0400

    refactor with callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,23 @@
 const request = require('request');
 
-let input = process.argv.slice(2)
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    // Print the error if one occurred
+    if (error !== null) {
+      callback(error, null);
+    } else if (response.statusCode !== 200) {
+    // Print the response status code if not 200
+      callback(response && response.statusCode, null);
+    } else if (typeof JSON.parse(body)[0] === 'undefined') {
+    // if no breed found
+      callback("No such breed", null);
+    } else {
+    // return desc
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+    }
+  });
+};
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + input, (error, response, body) => {
-  // Print the error if one occurred
-  if (error !== null) {
-    console.log('error:', error)
-  } else if (response.statusCode !== 200) {
-  // Print the response status code if not 200
-    console.log('statusCode:', response && response.statusCode);
-  } else if (typeof JSON.parse(body)[0] === 'undefined') {
-  // if no breed found 
-    console.log("No such breed")
-  } else {
-  // return desc
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  };
-});
+module.exports = { fetchBreedDescription };
+
